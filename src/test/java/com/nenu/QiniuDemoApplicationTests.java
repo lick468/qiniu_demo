317e1bb4c3a3a0f505839aa78035fefe30210280
lick468 <987723764@qq.com>
2020-06-17 12:09:14 +0800

    init

---
+++ src/test/java/com/nenu/QiniuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiniuDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
